c72d591a8a37b33102fa47652c526ebbe8387c67
ngtinh <nguyenvanductinh77@gmail.com>
2023-07-03 23:05:26 +0700

    Connect db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001;
@@ -7,6 +8,15 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
     return res.send('Xin chào');
 })
+
+
+mongoose.connect(`mongodb+srv://tinh:${process.env.MONGO_DB_PASS}@cluster0.oxcjy73.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Connected db successfully")
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(port, () => {
     console.log("Server listening on port " + port)
