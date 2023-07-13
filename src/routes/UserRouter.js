9e602040744e842c757ae965e5bfa6e7396928ca
ngtinh <nguyenvanductinh77@gmail.com>
2023-07-13 17:39:47 +0700

    Profile - Update User

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -7,7 +7,7 @@ const { authMiddleware, authUserMiddleware } = require('../middleware/authMiddle
 router.post('/sign-up', userController.createUser);
 router.post('/sign-in', userController.loginUser);
 router.post('/sign-out', userController.logoutUser);
-router.put('/update-user/:id', userController.UpdateUser);
+router.put('/update-user/:id', authMiddleware, userController.UpdateUser);
 router.delete('/delete-user/:id', authMiddleware, userController.DeteleUser);
 router.get('/getAll', authMiddleware, userController.getAllUser);
 router.get('/get-details/:id', authUserMiddleware, userController.getDetailsUser);
